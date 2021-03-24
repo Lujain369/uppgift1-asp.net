a0eb4257cedde72d70db0a40e986c4203a80f427
Lujain Al-shammari <lujain.alshammari@learnet.se>
2021-03-24 10:48:51 +0100

    working on model

---
+++ Uppgift1-ASP.NET/Models/Attender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uppgift1_asp.net.Model
+{
+    public class Attender
+    {
+        public int attender_id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone_number { get; set; }
+        public List<Event> events { get; set; }
+    }
+}
---
+++ Uppgift1-ASP.NET/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uppgift1_asp.net.Model
+{
+    public class Event
+    {
+        public int event_id { get; set; }
+        public int organisation_id { get; set; }
+        public string description { get; set; }
+        public string place { get; set; }
+        public string adress { get; set; }
+        public DateTime date { get; set; }
+        public int spots_available { get; set; }
+    }
+}
